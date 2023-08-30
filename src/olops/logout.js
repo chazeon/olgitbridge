a723b3f1150e630af0b3a7b100c8a6d60a07e98b
Chenxing Luo <hey@chazeon.com>
2023-08-30 17:41:54 -0400

    Fix logout error to work with overleaf version > 4
    On overleaf version >4, GET /logout returns 404,
    this will fail the olgitbridge server
    the client need to GET /project to get the csrf token instead.

--- src/olops/logout.js
+++ src/olops/logout.js
@@ -4,7 +4,7 @@
 module.exports =
 	async function( client, olServer )
 {
-	const res = await client.get( olServer + '/logout' );
+	const res = await client.get( olServer + '/project' );
 	const regexCSRF = /input name="_csrf" type="hidden" value="([^"]*)">/;
 	let csrf = res.data.match( regexCSRF )[ 1 ];
 	await client.post( olServer + '/logout', { '_csrf': csrf } );
